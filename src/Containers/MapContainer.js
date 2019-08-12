265a6387a00aff12e7a42fc9b3b8336b17c54edb
github username <dallasbille@gmail.com>
2019-08-12 16:01:04 -0400

    created Mapbox container, installed MapBox API, created the base for mapbox without API token

---
+++ src/Containers/MapContainer.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactMapGL from 'react-map-gl'
+
+class MapContainer extends React.Component {
+    state = {
+    viewport: {
+      width: 400,
+      height: 400,
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8
+    }
+  };
+
+    render(){
+        console.log(ReactMapGL);
+        return(
+            <div>
+            <ReactMapGL
+    {...this.state.viewport}
+    onViewportChange={(viewport) => this.setState({viewport})}
+  >Markers Here</ReactMapGL>
+
+            </div>
+        )
+    }
+}
+
+export default MapContainer
